1f78ec131208a2c304ee7eab99d5b90811a4e312
Alexandre Kopelevitch <contato@akop.com.br>
2023-09-10 15:40:54 -0300

    Added more CSS elements and some custom JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,4 +3,15 @@ import.meta.glob([
     '../fonts/**',
 ]);
 
+window.classToggle = (elementIds, classes) => {
+    if (typeof elementIds !== 'object' || typeof classes !== 'object') return false;
+    elementIds.forEach(elementId => {
+        let element = document.getElementById(elementId);
+        if (classes.length >= 1){
+            classes.forEach(classToToggle => {
+                element.classList.toggle(classToToggle)
+            })
+        }
+    });
+}
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,12 +13,16 @@
         <div class="left-side" id="left-sidebar">Olá</div>
         <div class="right-side">
             <nav>
-                <button class="square" onclick="document.getElementById('left-sidebar').classList.toggle('visible')">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
-                    </svg>
-                </button>
+                <span class="flat-button-with-icon" onclick="document.getElementById('left-sidebar').classList.toggle('slide-and-hide'); classToggle(['a','b'],['hidden'])">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="stroke" id='a' style="rotate: 90deg;">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                      </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="stroke hidden" id='b' style="rotate: 270deg">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                      </svg>
+
+
+                </span>
             </nav>
             <div class="content">
                 <div class="tile">
